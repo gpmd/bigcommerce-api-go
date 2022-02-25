1df523f89ebb157a797fbe022c8d5049b51a938a
Sam Ban <sam@gpmd.co.uk>
2022-02-25 11:40:33 +0000

    all webhook Data in struct so it's cleaner

--- webhook.go
+++ webhook.go
@@ -7,12 +7,38 @@ import (
 )
 
 type WebhookPayload struct {
-	Scope     string                 `json:"scope"`
-	StoreID   string                 `json:"store_id"`
-	Data      map[string]interface{} `json:"data"`
-	Hash      string                 `json:"hash"`
-	CreatedAt string                 `json:"created_at"`
-	Producer  string                 `json:"producer"`
+	Scope   string `json:"scope"`
+	StoreID string `json:"store_id"`
+	Data    struct {
+		Type     string `json:"type"`
+		ID       int64  `json:"id"`
+		CouponID string `json:"couponId"`
+		CartID   string `json:"cartId"`
+		OrderID  int64  `json:"orderId"`
+		Address  struct {
+			CustomerID int64 `json:"customer_id"`
+		} `json:"address"`
+		Inventory struct {
+			ProductID int64   `json:"product_id"`
+			Method    string  `json:"method"`
+			Value     float64 `json:"value"`
+			VariantID int64   `json:"variant_id"`
+		} `json:"inventory"`
+		Message struct {
+			OrderMessageID int64 `json:"order_message_id"`
+		} `json:"message"`
+		Sku struct {
+			ProductID int64 `json:"product_id"`
+			VariantID int64 `json:"variant_id"`
+		} `json:"sku"`
+		Status struct {
+			PreviousStatusID int64 `json:"previous_status_id"`
+			NewStatusID      int64 `json:"new_status_id"`
+		} `json:"status"`
+	} `json:"data"`
+	Hash      string `json:"hash"`
+	CreatedAt int64  `json:"created_at"`
+	Producer  string `json:"producer"`
 }
 
 // GetWebhookPayload returns a WebhookPayload object and the raw payload from the BigCommerce API
