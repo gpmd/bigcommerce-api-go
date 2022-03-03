be343f8bb4db8fa27b6dfaec42e81e6c40c0250a
Sam Ban <sam@gpmd.co.uk>
2022-03-03 13:31:33 +0000

    validate credentials

--- customer.go
+++ customer.go
@@ -1,5 +1,11 @@
 package bigcommerce
 
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
 // Customer is a struct for the BigCommerce Customer API
 type Customer struct {
 	ID               int64             `json:"id"`
@@ -19,4 +25,39 @@ type Customer struct {
 	ResetPassword    bool              `json:"reset_pass_on_login"`
 	AcceptsMarketing bool              `json:"accepts_marketing"`
 	Addresses        map[string]string `json:"addresses"`
+}
+
+func (bc *BigCommerce) ValidateCredentials(email, password string) (int64, error) {
+	var credReq struct {
+		Email     string `json:"email"`
+		Password  string `json:"password"`
+		ChannelID int    `json:"channel_id"`
+	}
+	credReq.Email = email
+	credReq.Password = password
+	credReq.ChannelID = bc.ChannelID
+	var b []byte
+	b, _ = json.Marshal(credReq)
+	req := bc.getAPIRequest(http.MethodPost, "/v3/customers/validate-credentials", bytes.NewBuffer(b))
+	res, err := bc.HTTPClient.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer res.Body.Close()
+	body, err := processBody(res)
+	if err != nil {
+		return 0, err
+	}
+	var credResptype struct {
+		IsValid    bool  `json:"is_valid"`
+		CustomerID int64 `json:"customer_id"`
+	}
+	err = json.Unmarshal(body, &credResptype)
+	if err != nil {
+		return 0, err
+	}
+	if !credResptype.IsValid {
+		return 0, ErrNotFound
+	}
+	return credResptype.CustomerID, nil
 }
