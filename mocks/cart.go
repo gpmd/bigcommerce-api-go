79b02548f2fc806c19f07147ba3355daae092a7f
Sam Ban <sam@gpmd.co.uk>
2022-03-07 15:12:03 +0000

    mocks for testing cart+login

--- mocks/cart.go
+++ mocks/cart.go
@@ -1,34 +1,84 @@
 package mocks
 
-import "github.com/gpmd/bigcommerce-api-go"
+import (
+	"strconv"
+
+	"github.com/gpmd/bigcommerce-api-go"
+)
 
 type CartClient struct {
+	carts       map[string]*bigcommerce.Cart
+	CartContent map[string]bigcommerce.LineItem
+	CartID      string
+	CustomerID  int64
+	Email       string
+	Password    string
 }
 
 func (cm *CartClient) CreateCart(items []bigcommerce.LineItem) (*bigcommerce.Cart, error) {
-	return nil, nil
+	cart := bigcommerce.Cart{
+		ID:         cm.CartID,
+		CustomerID: cm.CustomerID,
+	}
+	cart.LineItems.PhysicalItems = append(cart.LineItems.PhysicalItems, items...)
+	cm.carts[cart.ID] = &cart
+	return &cart, nil
 }
 
 func (cm *CartClient) GetCart(cartID string) (*bigcommerce.Cart, error) {
-	return nil, nil
+	if cart, ok := cm.carts[cartID]; ok {
+		return cart, nil
+	}
+	return nil, bigcommerce.ErrNotFound
 }
 
 func (cm *CartClient) CartAddItems(cartID string, items []bigcommerce.LineItem) (*bigcommerce.Cart, error) {
-	return nil, nil
+	if cart, ok := cm.carts[cartID]; ok {
+		cart.LineItems.PhysicalItems = append(cart.LineItems.PhysicalItems, items...)
+		return cart, nil
+	}
+	return nil, bigcommerce.ErrNotFound
 }
 
 func (cm *CartClient) CartEditItem(cartID string, item bigcommerce.LineItem) (*bigcommerce.Cart, error) {
-	return nil, nil
+	if cart, ok := cm.carts[cartID]; ok {
+		for i, lineItem := range cart.LineItems.PhysicalItems {
+			if lineItem.ID == item.ID {
+				cart.LineItems.PhysicalItems[i] = item
+				return cart, nil
+			}
+		}
+	}
+	return nil, bigcommerce.ErrNotFound
 }
 
 func (cm *CartClient) CartDeleteItem(cartID string, item bigcommerce.LineItem) (*bigcommerce.Cart, error) {
-	return nil, nil
+	if cart, ok := cm.carts[cartID]; ok {
+		for i, lineItem := range cart.LineItems.PhysicalItems {
+			if lineItem.ID == item.ID {
+				cart.LineItems.PhysicalItems = append(cart.LineItems.PhysicalItems[:i], cart.LineItems.PhysicalItems[i+1:]...)
+				return cart, nil
+			}
+		}
+	}
+	return nil, bigcommerce.ErrNotFound
 }
 
 func (cm *CartClient) CartUpdateCustomerID(cartID, customerID string) (*bigcommerce.Cart, error) {
-	return nil, nil
+	if cart, ok := cm.carts[cartID]; ok {
+		cid, err := strconv.ParseInt(customerID, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		cart.CustomerID = cid
+		return cart, nil
+	}
+	return nil, bigcommerce.ErrNotFound
 }
 
 func (cm *CartClient) ValidateCredentials(email, password string) (int64, error) {
-	return 0, nil
+	if email == cm.Email && password == cm.Password {
+		return 1, nil
+	}
+	return 0, bigcommerce.ErrNotFound
 }
